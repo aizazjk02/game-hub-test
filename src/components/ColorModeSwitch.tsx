c358b728c342714f849a72f30e62a2c7174309fe
Aizaz <aizazkotwal2204@outlook.com>
2024-03-29 15:28:44 +0530

    feat: add toggle dark mode support.

---
+++ src/components/ColorModeSwitch.tsx
@@ -0,0 +1,14 @@
+import { HStack, Switch, Text, useColorMode } from "@chakra-ui/react"
+
+
+const ColorModeSwitch = () => {
+    const { toggleColorMode, colorMode } = useColorMode()
+    return (
+        <HStack>
+            <Switch colorScheme="blue" isChecked={colorMode === "dark"} onChange={toggleColorMode} />
+            <Text whiteSpace={"nowrap"}>{colorMode} Mode</Text>
+        </HStack>
+    )
+}
+
+export default ColorModeSwitch
