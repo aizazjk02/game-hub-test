c134064260bcb15007e2e0cb22423b072a442888
Aizaz <aizazkotwal2204@outlook.com>
2024-04-08 16:42:14 +0530

    refactor: handle games with no trailers.

--- src/components/GameTrailer.tsx
+++ src/components/GameTrailer.tsx
@@ -1,4 +1,4 @@
-import { Spinner } from "@chakra-ui/react";
+import { Spinner, Text } from "@chakra-ui/react";
 import useTrailers from "../hooks/useTrailers"
 
 interface Props {
@@ -6,10 +6,19 @@ interface Props {
 }
 const GameTrailer = ({ gameId }: Props) => {
     const { data, isLoading, error } = useTrailers(gameId)
+    console.log(gameId)
     if (isLoading) return <Spinner />
     if (error) throw error
     const stream = data?.results[0]
-    return (<video src={stream?.data[480]} poster={stream?.preview} controls />)
+    return (
+        <>
+            <Text marginY={2}>Trailer: </Text>
+            {stream ?
+                <video src={stream?.data[480] || stream?.data["max"]} poster={stream?.preview} controls />
+                : (<Text>There are no trailers available.</Text>)
+            }
+        </>
+    )
 }
 
 export default GameTrailer
