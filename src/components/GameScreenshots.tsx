7237896aee8c9c1b822dc0a0e1e48567c50e28c1
Aizaz <aizazkotwal2204@outlook.com>
2024-04-08 16:41:35 +0530

    feat: add game screenshots component.

--- src/components/GameScreenshots.tsx
+++ src/components/GameScreenshots.tsx
@@ -1,22 +1,25 @@
-import { Image, SimpleGrid, Spinner } from "@chakra-ui/react";
+import { Box, Image, SimpleGrid, Spinner, Text } from "@chakra-ui/react";
 import useScreenshots from "../hooks/useScreenshots";
 
 interface Props {
-    gameId:number;
+    gameId: number;
 }
 
-const GameScreenshots = ({gameId}:Props) => {
-    const {data, isLoading, error} = useScreenshots(gameId)
-    if(isLoading) return <Spinner/>
-    if(error) throw error
+const GameScreenshots = ({ gameId }: Props) => {
+    const { data, isLoading, error } = useScreenshots(gameId)
+    if (isLoading) return <Spinner />
+    if (error) throw error
 
-  return (
-    <SimpleGrid columns={{base: 1, md: 2}} spacing={2}>
-        {data?.results.map(file => (
-            <Image key={file.id} src={file.image}/>
-        ))}
-    </SimpleGrid>
-  )
+    return (
+        <Box marginTop={2}> 
+            <Text>Screenshots: </Text>
+            <SimpleGrid columns={{ base: 1, md: 2 }} spacing={2}>
+                {data?.results.map(file => (
+                    <Image key={file.id} src={file.image} />
+                ))}
+            </SimpleGrid>
+        </Box>
+    )
 }
 
 export default GameScreenshots
