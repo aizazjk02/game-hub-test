a15d131b1e0187bf834c36999eec88795105f966
Aizaz <aizazkotwal2204@outlook.com>
2024-04-08 16:53:30 +0530

    fix: add navigate to the search box.

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -2,14 +2,17 @@ import { Input, InputGroup, InputLeftElement } from "@chakra-ui/react"
 import { useRef } from "react"
 import { BsSearch } from "react-icons/bs"
 import useGameQueryStore from "../store"
+import { useNavigate } from "react-router-dom"
 
 const SearchInput = () => {
     const searchInputRef = useRef<HTMLInputElement>(null)
     const setSearchText = useGameQueryStore(store => store.setSearchText)
+    const navigate = useNavigate()
     return (
         <form onSubmit={(event) => {
             event.preventDefault()
             if (searchInputRef.current) setSearchText(searchInputRef.current.value)
+            navigate("/")
         }}>
 
             <InputGroup>
