e40e918dd60a385c94153801774823796e6e8495
Aizaz <aizazkotwal2204@outlook.com>
2024-04-08 15:50:05 +0530

    feat: add game attributes component.

--- src/entities/Game.ts
+++ src/entities/Game.ts
@@ -1,4 +1,6 @@
+import { Genre } from "./Genre";
 import { Platform } from "./Platform";
+import { Publisher } from "./Publisher";
 
 // Define the Game interface
 export interface Game {
@@ -6,6 +8,8 @@ export interface Game {
   name: string;
   background_image: string;
   parent_platforms: { platform: Platform; }[];
+  genres: Genre[];
+  publishers: Publisher[];
   rating: number;
   rating_top: number;
   slug: string;
--- src/pages/GameDetailsPage.tsx
+++ src/pages/GameDetailsPage.tsx
@@ -1,15 +1,16 @@
-import { useParams } from "react-router-dom"
-import useGame from "../hooks/useGame"
 import { Box, Heading, Spinner } from "@chakra-ui/react"
+import { useParams } from "react-router-dom"
 import ExpandableText from "../components/ExpandableText"
+import GameAttributes from "../components/GameAttributes"
+import useGame from "../hooks/useGame"
 
 const GameDetailsPage = () => {
-    const {slug} = useParams()
-    const {data: game, isLoading, error} = useGame(slug!)
+    const { slug } = useParams()
+    const { data: game, isLoading, error } = useGame(slug!)
 
-    if(isLoading) return <Spinner/>
-    if(error || !game) throw error
-    return(
+    if (isLoading) return <Spinner />
+    if (error || !game) throw error
+    return (
         <Box>
             <Heading>
                 {game.name}
@@ -17,6 +18,7 @@ const GameDetailsPage = () => {
             <ExpandableText>
                 {game.description_raw}
             </ExpandableText>
+            <GameAttributes game={game}/>
         </Box>
     )
 }
