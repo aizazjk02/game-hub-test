05deac7988cb5ac99de66d7b53f7b7e1f4a05349
Aizaz <aizazkotwal2204@outlook.com>
2024-04-08 16:42:39 +0530

    refactor: change game details page layout.

--- src/pages/GameDetailsPage.tsx
+++ src/pages/GameDetailsPage.tsx
@@ -1,30 +1,34 @@
-import { Box, Heading, Spinner } from "@chakra-ui/react"
-import { useParams } from "react-router-dom"
-import ExpandableText from "../components/ExpandableText"
-import GameAttributes from "../components/GameAttributes"
-import useGame from "../hooks/useGame"
-import GameTrailer from "../components/GameTrailer"
-import GameScreenshots from "../components/GameScreenshots"
+import { Box, GridItem, Heading, SimpleGrid, Spinner } from "@chakra-ui/react";
+import { useParams } from "react-router-dom";
+import ExpandableText from "../components/ExpandableText";
+import GameAttributes from "../components/GameAttributes";
+import useGame from "../hooks/useGame";
+import GameTrailer from "../components/GameTrailer";
+import GameScreenshots from "../components/GameScreenshots";
 
 const GameDetailsPage = () => {
-    const { slug } = useParams()
-    const { data: game, isLoading, error } = useGame(slug!)
+  const { slug } = useParams();
+  const { data: game, isLoading, error } = useGame(slug!);
 
-    if (isLoading) return <Spinner />
-    if (error || !game) throw error
-    return (
+  if (isLoading) return <Spinner />;
+  if (error || !game) throw error;
+  return (
+    <SimpleGrid columns={{ base: 1, md: 2 }} spacing={5}>
+      <GridItem>
+        <Heading>{game.name}</Heading>
+        <ExpandableText>{game.description_raw}</ExpandableText>
+        <GameAttributes game={game} />
+      </GridItem>
+      <GridItem>
         <Box>
-            <Heading>
-                {game.name}
-            </Heading>
-            <ExpandableText>
-                {game.description_raw}
-            </ExpandableText>
-            <GameAttributes game={game}/>
-            <GameTrailer gameId={game.id}/>
-            <GameScreenshots gameId={game.id}/>
+          <GameTrailer gameId={game.id} />
         </Box>
-    )
-}
+        <Box>
+          <GameScreenshots gameId={game.id} />
+        </Box>
+      </GridItem>
+    </SimpleGrid>
+  );
+};
 
-export default GameDetailsPage
+export default GameDetailsPage;
